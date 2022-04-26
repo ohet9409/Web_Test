da4402aa2b54b3acc9d6486ea380f8c09331bc48
oet <54170438+ohet9409@users.noreply.github.com>
2022-04-26 22:41:33 +0900

    p.118 ~ p.133

--- Todo/src/main/java/com/example/demo/service/TodoService.java
+++ Todo/src/main/java/com/example/demo/service/TodoService.java
@@ -1,11 +1,30 @@
 package com.example.demo.service;
 
+import com.example.demo.TodoRepository;
+import com.example.demo.model.TodoEntity;
+import com.sun.tools.javac.comp.Todo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class TodoService {
 
-    public String testService(){
-        return "Test Service";
+    @Autowired
+    private TodoRepository repository;
+
+    public String testService() {
+        // TodoEntity 생성
+        TodoEntity entity = TodoEntity.builder().title("My first todo item").build();
+        // TodoEntity 저장
+        repository.save(entity);
+        System.out.println(entity.toString());
+
+
+        // TodoEntity 검색
+        TodoEntity savedEntity = repository.findById(entity.getId()).get();
+
+        //return "Test Service";
+        return savedEntity.getTitle() + ", " + savedEntity.getId() + ", " + savedEntity.getUserId();
+//        return savedEntity.getTitle();
     }
 }
---
+++ Todo/src/main/webapp/WEB-INF/views/index.jsp
@@ -0,0 +1,13 @@
+<%@ page language="java" contentType="text/html; charset=EUC-KR"
+    pageEncoding="EUC-KR"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="EUC-KR">
+<title>Insert title here</title>
+</head>
+<body>
+	<h3>index.jsp</h3>
+	<a href="t1">링크 1</a>
+</body>
+</html>
